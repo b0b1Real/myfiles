c1139161d349746ae9072a1d6be0eed654c69143
b0b1Real <bobigaming12345@gmail.com>
2025-04-09 13:55:26 +0000

    Add files via upload

---
+++ templates/cp.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define hashmap unordered_map
+#define forn(i, n) for(int i = 0;i<int(n);i++)
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 10001;
+
+int main(){
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	
+	return 0;
+}
